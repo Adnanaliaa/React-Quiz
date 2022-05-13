e41bd5ef467ef2a0bf8ac2c4642095eaf0dbc782
Adnan Ali <adnan.ahmed.9620@gmail.com>
2022-05-13 18:12:22 +0500

    Almost done

---
+++ src/src/App.js
@@ -0,0 +1,83 @@
+import { Button, Typography } from "@mui/material";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  const [marks, setMarks] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const [questions, setQuestions] = useState([
+    {
+      question: "What does HTML stand for",
+      options: ["HYpertex", "wrong", "wrong", "wrong"],
+      correctAnswers: "HYpertex",
+    },
+    {
+      question: "What does CSS stand for",
+      options: ["wrong", "CSS", "wrong", "wrong"],
+      correctAnswers: "CSS",
+    },
+        
+  ]);
+  const [questionInd, setQuestionInd] = useState(0);
+
+  const nextQuestion = () => {
+    if (questions.length - 1 == questionInd) {
+      setShowResult(true);
+    } else {
+      setQuestionInd(questionInd + 1);
+    }
+  };
+  const checkAnswer = (cor, user) => {
+    if (cor == user) {
+      setMarks(marks + 1);
+    }
+    nextQuestion();
+  };
+
+  return (
+    <div className="App">
+      <MenuAppBar />
+      {showResult ? (
+        <Grid>
+          <Paper elevation={5}>
+            <Typography variant="h3">
+              {(marks / questions.length) * 100} %
+            </Typography>
+          </Paper>
+        </Grid>
+      ) : (
+        <Grid>
+          <Grid spacing={3} container>
+            <Grid md={12}>
+              <Typography className="py-5" variant="h5">
+                {questions[questionInd].question}
+              </Typography>
+            </Grid>
+            {questions[questionInd].options.map((e, i) => (
+              <Grid key={i} className="p-3" md={6}>
+                <Button
+                  onClick={() =>
+                    checkAnswer(questions[questionInd].correctAnswers, e)
+                  }
+                  variant="contained"
+                >
+                  {e}
+                </Button>
+              </Grid>
+            ))}
+
+            <Grid className="p-3" md={12}>
+              <Button onClick={() => nextQuestion()} variant="outlined">
+                Next
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      )}
+    </div>
+  );
+}
+
+export default App;
